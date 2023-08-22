f4b83da2e97ebee37d679939add517f103f735b0
wanjiku <annritawanjiku77@gmail.com>
2023-08-22 08:15:13 +0300

    added message form

--- pages/index.js
+++ pages/index.js
@@ -1,7 +1,10 @@
 export default function Home() {
   return (
-    <>
-
-    </>
+    <main>
+      {/* form for message input */}
+      <input type="text" placeholder="Write message here"/>
+      <div>captcha</div>
+      <button>send</button>
+    </main>
   )
 }
