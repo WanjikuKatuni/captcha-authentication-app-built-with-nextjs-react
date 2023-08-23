a0556ebb2814825f919306a32d4b92110264a1ac
wanjiku <annritawanjiku77@gmail.com>
2023-08-23 07:37:19 +0300

    adding session(iron session)

--- pages/api/captcha-image.js
+++ pages/api/captcha-image.js
@@ -2,8 +2,41 @@
 
 import fs from 'fs'
 
-export default function handler(req, res) {
+
+import { withIronSessionApiRoute } from "iron-session/next";
+
+
+
+function handler(req, res) {
+    const index = req.query.index;
+    // console.log({index})
+
+    // array of images
+    const images = []
+
     res.setHeader( 'Content-Type', 'image/png');
     const imageBuffer = fs.readFileSync('./public/muffinsndogs/dog1.png')
     res.send(imageBuffer)
 }
+
+export default withIronSessionApiRoute(
+    function handler(req, res) {
+        const index = req.query.index;
+        // console.log({index})
+
+        // Check if there are images in session
+        if(!req.session.captchaImages){
+            // array of images
+            req.session.captchaImages = []
+        }
+    
+        res.setHeader( 'Content-Type', 'image/png');
+        const imageBuffer = fs.readFileSync('./public/muffinsndogs/dog1.png')
+        res.send(imageBuffer)
+    },
+    {
+        cookieName: "session",
+        password: process.env.SESSION_SECRET,
+    }
+ 
+)
