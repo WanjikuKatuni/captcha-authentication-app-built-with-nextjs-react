a78e52f091d1630072ad748a463ae2a793d856f4
wanjiku <annritawanjiku77@gmail.com>
2023-08-25 08:04:41 +0300

    added functionality for selected images and adjusted their css

--- components/Captcha.js
+++ components/Captcha.js
@@ -1,21 +1,45 @@
 // captcha component
 
-import React from 'react'
+import React, { useState } from 'react'
 
-const arrayLength = 9;
-const imageLocations = Array(arrayLength).fill(null).map((value, index) => {
-  return `/api/captcha-image?index=${index}`;
-})
+
 
 export const Captcha = () => {
+  const [selectedIndexes, setSelectedIndexes] = useState([])
+  const arrayLength = 9;
+  const imageLocations = Array(arrayLength).fill(null).map((value, index) => {
+  return `/api/captcha-image?index=${index}`;
+  })
+  // onCLick function to toggle selected images
+  function toggleIndex(index){
+    // alert(index)
+
+    // save index inside of selected indexes
+    setSelectedIndexes(prev => {
+      if(prev.includes(index)){
+        // removes from array if already selected
+        return prev.filter(v => v !== index)
+      } else {
+        // adds to array if not selected
+        return [...prev, index]
+      }
+    })
+  }
   return (
     <div className='captcha'>
       <h2>To confirm you are not a robot, select all images of dogs</h2>
       <div className='captcha-images'>
         {/* map captcha images */}
-        {imageLocations.map(imageUrl => (
-          <div>
+        {imageLocations.map((imageUrl, index) => (
+          <div 
+
+          onClick={()=> toggleIndex(index)}
+
+          // setstate for selected divs with images
+          className={selectedIndexes.includes(index) ? 'selected' : ''}>
+
             <img src={imageUrl} alt=''/>
+
           </div>
         ))}
 
