8dd7b5bb7c218ee27b34a75cb4f2c5d847a78bd9
wanjiku <annritawanjiku77@gmail.com>
2023-08-25 07:07:24 +0300

    saving random images to session

--- pages/api/captcha-image.js
+++ pages/api/captcha-image.js
@@ -1,37 +1,51 @@
 // api endpoint
 
 import fs from 'fs'
-
-
 import { withIronSessionApiRoute } from "iron-session/next";
 
 
 
-function handler(req, res) {
-    const index = req.query.index;
-    // console.log({index})
+const dogProbability = 0.5;
 
-    // array of images
-    const images = []
+function newCaptchaImages(){
+    return new Array(9)
+    .fill(null)
+    .map((val, index) => {
 
-    res.setHeader( 'Content-Type', 'image/png');
-    const imageBuffer = fs.readFileSync('./public/muffinsndogs/dog1.png')
-    res.send(imageBuffer)
+        // find random number and compare it with dog probability. If less than dog probability, save it on shouldbedog variable
+        const shouldBeDog = Math.random() < dogProbability
+
+        // generate random numbers equivalent to the amount of numbers in images
+        let number;
+        if (shouldBeDog){
+            number = Math.floor(Math.random() * 10) + 1
+        } else {
+            number = Math.floor(Math.random() * 13) + 1
+        }
+
+
+        const fileName = (shouldBeDog ? 'dog' : 'muffin') + number + '.png' 
+        
+        return `./public/muffinsndogs/${fileName}`
+    })
 }
 
+
 export default withIronSessionApiRoute(
-    function handler(req, res) {
+    async function handler(req, res) {
         const index = req.query.index;
         // console.log({index})
 
         // Check if there are images in session
         if(!req.session.captchaImages){
-            // array of images
-            req.session.captchaImages = []
+            // save array of images
+            req.session.captchaImages = newCaptchaImages();
+
+            await req.session.save()
         }
     
         res.setHeader( 'Content-Type', 'image/png');
-        const imageBuffer = fs.readFileSync('./public/muffinsndogs/dog1.png')
+        const imageBuffer = fs.readFileSync( req.session.captchaImages[index])
         res.send(imageBuffer)
     },
     {
